b68be51012a613165298e690d42dbfc2bbbb156f
wadexhong <wadexhong@gmail.com>
2018-06-27 14:08:17 +0800

    SearchView init.

--- app/src/main/java/com/wadexhong/chocolabs/mainpage/MainActivity.java
+++ app/src/main/java/com/wadexhong/chocolabs/mainpage/MainActivity.java
@@ -1,7 +1,13 @@
 package com.wadexhong.chocolabs.mainpage;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import com.wadexhong.chocolabs.R;
 
@@ -9,12 +15,44 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
 
     private MainContract.Presenter mPresenter;
 
+    private Toolbar mToolbar;
+    private SearchView mSearchView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         mPresenter = new MainPresenter(this);
+
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        mToolbar.setTitleTextColor(getResources().getColor(R.color.white));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        mSearchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        mSearchView.setIconifiedByDefault(true);
+        mSearchView.setQueryHint("請輸入文字");
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
